74feb058d8afe9071e4f4303394b63b13ed5df2a
Liloa <uc@hawaii.edu>
2020-11-10 18:35:17 -1000

    wip

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App" id = "mainDiv" style={{height:"100%", width:"70%", marginLeft:'auto', marginRight:'auto'}}>
-        <iframe height="100%" width="100%" src="https://console.dialogflow.com/api-client/demo/embedded/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
+        // <iframe height="100%" width="100%" src="https://console.dialogflow.com/api-client/demo/embedded/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
+
+        
     </div>
   );
 }
