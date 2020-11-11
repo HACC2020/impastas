18b5e43321c53e5245bc473cb6196435f7511648
Kahlin Baughman <kahlin@hawaii.edu>
2020-11-10 18:03:36 -1000

    Test iframe input

--- ui/src/App.js
+++ ui/src/App.js
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div >
+      <iframe height="430" width="350" src="https://bot.dialogflow.com/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
+      </div>
     </div>
   );
 }
