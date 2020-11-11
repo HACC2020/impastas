701cadb361a00d4f6e62da227a08abb0fc284fbd
Liloa <uc@hawaii.edu>
2020-11-10 20:39:53 -1000

    big button

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,15 +6,14 @@ function App() {
   const [toggleView, setToggleView] = useState(false);
   return (
     <div className="App flex" id = "mainDiv" style={{height:"100%", width:"100%", marginLeft:'auto', marginRight:'auto'}}>
-  <div onClick={()=>setToggleView(!toggleView)}>
-     Click me
-   </div>
+    <button type="button" onClick={()=>setToggleView(!toggleView)} class="btn btn-info">Click me to Change</button>
+
    {
      toggleView
      ?<iframe height="100%" width="70%" src="https://console.dialogflow.com/api-client/demo/embedded/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
      :
      <>
-     <iframe class="iframe" src="https://health.hawaii.gov/coronavirusdisease2019/" width="100%" height="100%"></iframe>
+     <iframe class="iframe" src="https://hawaiicovid19.com/" width="100%" height="100%"></iframe>
 
      <df-messenger
   chat-icon="https://storage.googleapis.com/cloudprod-apiai/5f976cc0-271e-4902-a38f-e56bd3434466_x.png"
