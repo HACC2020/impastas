113a319e7b49efd235094a82e2ce123890a1f996
Liloa <uc@hawaii.edu>
2020-11-10 20:35:38 -1000

    big brain time

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,20 +5,27 @@ import {useState} from 'react';
 function App() {
   const [toggleView, setToggleView] = useState(false);
   return (
-    <div className="App flex" id = "mainDiv" style={{height:"100%", width:"70%", marginLeft:'auto', marginRight:'auto'}}>
+    <div className="App flex" id = "mainDiv" style={{height:"100%", width:"100%", marginLeft:'auto', marginRight:'auto'}}>
   <div onClick={()=>setToggleView(!toggleView)}>
      Click me
    </div>
    {
      toggleView
-     ?<iframe height="100%" width="100%" src="https://console.dialogflow.com/api-client/demo/embedded/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
-     :<df-messenger
+     ?<iframe height="100%" width="70%" src="https://console.dialogflow.com/api-client/demo/embedded/8ccb78e0-5510-4842-9fe3-2b0447d3ef67"></iframe>
+     :
+     <>
+     <iframe class="iframe" src="https://health.hawaii.gov/coronavirusdisease2019/" width="100%" height="100%"></iframe>
+
+     <df-messenger
   chat-icon="https://storage.googleapis.com/cloudprod-apiai/5f976cc0-271e-4902-a38f-e56bd3434466_x.png"
   intent="WELCOME"
   chat-title="MedBae"
   agent-id="8ccb78e0-5510-4842-9fe3-2b0447d3ef67"
   language-code="en"
-></df-messenger>
+    ></df-messenger>
+     </>
+
+     
    }
         
 
